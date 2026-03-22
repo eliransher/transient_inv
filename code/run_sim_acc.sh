ff021d6f5812700dd9f6ff9653fa449d7972a3a2
eliransher <scherzter@gmail.com>
2026-03-22 15:38:24 +0200

    initail comit

---
+++ code/run_sim_acc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -t 0-03:58
+#SBATCH -A def-dkrass
+#SBATCH --mem 20000
+source /home/eliransc/projects/def-dkrass/eliransc/mom_match/bin/activate
+python /home/eliransc/projects/def-dkrass/eliransc/transient_inv/inventory_accuracy_trials.py --trials 10 --replications 500 --dump-dir /scratch/eliransc/elad_trans
