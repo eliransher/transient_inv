4681e11c112b74c8a986bc74cef95ea07e522617
eliransher <scherzter@gmail.com>
2026-03-23 14:48:49 +0200

    initail comit

--- code/sim_inv_trans.sh
+++ code/sim_inv_trans.sh
@@ -3,4 +3,4 @@
 #SBATCH -A def-dkrass
 #SBATCH --mem 20000
 source /home/eliransc/projects/def-dkrass/eliransc/mom_match/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/transient_inv/code/inventory_simpy_ph.py --dynamic-demand --n-settings 3000 --replications 75000 --horizon 100 --inv-dir /scratch/eliransc/elad_trans/train_data/inv_level --order-dir /scratch/eliransc/elad_trans/train_data/order --loss-dir /scratch/eliransc/elad_trans/train_data/loss
+python /home/eliransc/projects/def-dkrass/eliransc/transient_inv/code/inventory_simpy_ph.py --dynamic-demand --n-settings 400 --replications 75000 --horizon 100 --inv-dir /scratch/eliransc/elad_trans/train_data/inv_level --order-dir /scratch/eliransc/elad_trans/train_data/order --loss-dir /scratch/eliransc/elad_trans/train_data/loss
