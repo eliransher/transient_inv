d91f6d08defb5f5c1201687ae19ba1e4cafdd837
eliransher <scherzter@gmail.com>
2026-08-11 12:56:17 +0300

    initail comit

--- code/mpi_parallel.sh
+++ code/mpi_parallel.sh
@@ -8,4 +8,4 @@
 
 module load gcc openmpi hdf5/serial python/3
 source /home/elirans/project/queues/bin/activate
-mpirun python3 //home/elirans/project/transient_inv/code/inventory_simpy_ph.py --partition-counts-csv val_set_counts.csv --partition-simulations 5000 --replications 110000 --horizon 100 --inter-size 100 --lead-size 100 --control-max-tries 1000 --inv-dir /home/elirans/scratch/elad_trans/training_data/inv_level_1 --order-dir /home/elirans/scratch/elad_trans/training_data/order_1 --loss-dir /home/elirans/scratch/elad_trans/training_data/loss_1
+mpirun python3 //home/elirans/project/transient_inv/code/inventory_simpy_ph.py --partition-counts-csv val_set_counts.csv --partition-simulations 5000 --replications 110000 --horizon 100 --inter-size 100 --lead-size 100 --control-max-tries 1000 --inv-dir /home/elirans/scratch/elad_trans/training_data/inv_level_2 --order-dir /home/elirans/scratch/elad_trans/training_data/order_2 --loss-dir /home/elirans/scratch/elad_trans/training_data/loss_2
